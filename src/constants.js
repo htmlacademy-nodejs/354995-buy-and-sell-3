c7b64fb4d941d406ed428fa8a05a6cbca33a57a9
Alexander Matskevich <a.matskevich@studionx.ru>
2021-10-29 15:09:39 +0300

    Chalk added, async writeFile instead promises added.

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const DEFAULT_COMMAND = `--help`;
+
+const USER_ARGV_INDEX = 2;
+
+const ExitCode = {
+  ERROR: 1,
+  SUCCESS: 0,
+};
+
+module.exports = {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  ExitCode,
+};
