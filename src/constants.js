8299c9dab06a4b0dea49350fe322ad1e51994c3f
Alexander Matskevich <a.matskevich@studionx.ru>
2021-11-01 11:19:31 +0300

    Added nodemon, server create file.

--- src/constants.js
+++ src/constants.js
@@ -14,3 +14,11 @@ module.exports = {
   USER_ARGV_INDEX,
   ExitCode,
 };
+
+module.exports.HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
